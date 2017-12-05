78c1023ca4f08c2e4752e4b1056a8047a9c2896c
Eric Higgins <erichiggins@gmail.com>
2017-12-04 19:25:19 -0800

    Add existing files to index.js. Good enough to be usable

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-exports.cartridges = require('./cartridges.json');
+exports.pistol = {};
+exports.rifle = {};
+exports.rimfire = {};
+exports.shotshell = {};
+
+exports.names = require('./cartridges.json');
+
+exports.pistol.saami = require('./pistol/saami.json');
+exports.rifle.saami = require('./rifle/saami.json');
+exports.rimfire.saami = require('./rimfire/saami.json');
+exports.shotshell.saami = require('./shotshell/saami.json');
